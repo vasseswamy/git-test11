dd653c45e5312045f82cb3ab80056e14bb384ff8
swamy <swamy.v@tessrac.com>
2022-05-19 15:09:45 +0530

    initial commit

---
+++ form-validation1/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Form from "./components/Form";
+
+const App = () => {
+  return (
+    <div>
+      <Form />
+    </div>
+  )
+}
+
+
+export default App;
---
+++ form-validation1/src/components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignupForm from './SignupForm';
+
+const Form = () => {
+  return (
+    <div>
+        <SignupForm />
+    </div>
+  )
+}
+
+export default Form;
